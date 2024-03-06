4ead1a1cb4cc492e4a71b83a24efbeeb22762cbc
watanuki <luminoxmiira@gmail.com>
2024-03-06 23:48:28 +0900

    ADD:stopping server Action

--- app/shadcn-ui_demo/src/app/containers/page.tsx
+++ app/shadcn-ui_demo/src/app/containers/page.tsx
@@ -4,19 +4,29 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { ContainerInfo } from "@/lib/types";
 
 async function getContainerList() {
-    const responce = await fetch("http://express_apiserver:3000/dockerps", {
-        cache: "no-store",
-    });
+    try{
+        const responce = await fetch("http://express_apiserver:3000/dockerps", {
+            cache: "no-store",
+        });
 
-    const ContainerListData: ContainerInfo[] = await responce.json();
+        if (!responce.ok) {
+            throw new Error(`API Server is stopping now. (&{reaponse.Status})`);
+        }
 
-    return ContainerListData;
+        const ContainerListData: ContainerInfo[] = await responce.json();
+        return ContainerListData;
+    } catch (Error) {
+        const ContainerListData: ContainerInfo[] = await [];
+        return ContainerListData;
+    }
+
+
 }
 
 export default async function Page() {
     const ContainerListData = await getContainerList();
 
-    console.log(ContainerListData);
+    console.log(ContainerListData.length);
 
     return(
         <>
@@ -27,13 +37,17 @@ export default async function Page() {
                         <h1 className="font-bold">Container List</h1>
                     </div>
                     <ScrollArea className="w-full h-full gap-4">
-                        {ContainerListData.map((ContainerDetails: ContainerInfo) => (
-                            <ContainerCard key={ContainerDetails.ID} ContainerDetails={ContainerDetails}/>
-                        ))}
+                        {ContainerListData.length > 0 ? (
+                            ContainerListData.map((ContainerDetails: ContainerInfo) => (
+                                <ContainerCard key={ContainerDetails.ID} ContainerDetails={ContainerDetails}/>
+                            ))) : (
+                                <p className="text-center w-full py-[100%]">API Server is stopped.</p>
+                            )
+                        }
                     </ScrollArea>
                 </div>
                 <div className="flex-none w-[73%]">
-
+                        <p className="text-center py-[40%]">Not Selected.</p>
                 </div>
             </div>
         </div>
